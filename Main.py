e54c42dfbed8b9812723671b7b7d6efd607c8ce6
Mark <Mark@Macbookflanker.local>
2023-12-10 20:16:54 +0300

    Main

---
+++ Main.py
@@ -0,0 +1,12 @@
+from Classes import LVM
+
+x0 = int(input('Start number of prey:'))
+y0 = int(input('Start number of predators:'))
+alpha = float(input('Prey population growth rate:'))
+beta = float(input('Hunting rate:'))
+gamma = float(input('Prey consumtion rate:'))
+delta = float(input('Predators population growth rate:'))
+
+g = LVM(alpha, beta, gamma, delta, x0, y0)
+g.solution()
+g.draw()
