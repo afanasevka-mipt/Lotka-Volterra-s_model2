4a5e0b76f17b134d6be5b12bae22435e0584145a
Mark <Mark@Macbookflanker.local>
2023-12-10 20:12:58 +0300

    ready classes

--- Classes.py
+++ Classes.py
@@ -10,7 +10,7 @@ class LVM(object):
         self.x0 = x0
         self.y0 = y0
 
-    def fun_vec(self, r, t):
+    def fun_vec(self, r, t): #Определение функции-вектора правых частей системы
         x = r[0]
         y = r[1]
         fx = self.alpha*x - self.beta*x*y
@@ -18,7 +18,7 @@ class LVM(object):
         self.fz = np.array([fx, fy], float)
         return self.fz
     
-    def solution(self):
+    def solution(self): # Решение уравнения
         a = 0
         b = 1000
         N = 1000
@@ -28,7 +28,7 @@ class LVM(object):
         self.ypoints = []
         r = np.array([self.x0, self.y0], float)
         
-        for t in self.time:
+        for t in self.time: #Основной цикл вычисления по методу Рунге-Кутты
             self.xpoints.append(r[0])
             self.ypoints.append(r[1])
             k1 = h*self.fun_vec(r, t)
@@ -38,3 +38,13 @@ class LVM(object):
             r += (k1+2.*k2+2.*k3+k4)/6.
         
         return self.time, self.xpoints, self.ypoints
+    
+    def draw(self): #График
+        plt.plot(self.time, self.xpoints, 'g', label='Prey')
+        plt.plot(self.time, self.ypoints, 'r', label='Predator')
+        plt.xlabel('Time')
+        plt.ylabel('Population number')
+        plt.title("Lotka-Volterra's model")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
