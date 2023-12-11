70fb69c3083b4cfed057697e722272b512091d53
Mark <Mark@Macbookflanker.local>
2023-12-11 16:38:23 +0300

    Main_final

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ x0 = int(input('Start number of prey:'))
 y0 = int(input('Start number of predators:'))
 alpha = float(input('Prey population growth rate:'))
 beta = float(input('Hunting rate:'))
-gamma = float(input('Prey consumtion rate:'))
+gamma = float(input('Predators population decrease rate:'))
 delta = float(input('Predators population growth rate:'))
 
 g = LVM(alpha, beta, gamma, delta, x0, y0)
